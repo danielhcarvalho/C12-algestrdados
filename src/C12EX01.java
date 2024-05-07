914506e6730848b8609a765a0c33aea3e6b3d8dc
Daniel Henrique de Carvalho <danhcarv.alves@gmail.com>
2024-05-07 20:32:18 -0300

    Correção do exercício C12EX01

--- src/C12EX01.java
+++ src/C12EX01.java
@@ -15,15 +15,14 @@ public class C12EX01
             "Informe o número " + (i + 1) + ": ",
             "",
             JOptionPane.QUESTION_MESSAGE));
-        }
-        for (int i = 0; i < num.length; i++) 
-        {
+            
             if (num[i] != 0 && num[i] % 5 == 0 && num[i] % 7 == 0) 
             {
-                    numSelecionados.add(num[i]); 
+                numSelecionados.add(num[i]); 
 
             }
-        }         
+        }  
+              
         if (!numSelecionados.isEmpty())
         {
          System.out.printf("Foram digitados %d números divisíveis por 5 e 7\n%d -- %d -- %d", numSelecionados.size(), numSelecionados.get(0), numSelecionados.get(1), numSelecionados.get(2));
