a6b54559070167b305121d8b017a377f0f606d30
Daniel Henrique de Carvalho <danhcarv.alves@gmail.com>
2024-05-07 20:10:30 -0300

    Conclusão do exercício C12EX01

--- src/App.java
+++ src/App.java
@@ -1,27 +1,36 @@
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 public class App 
 {
     public static void main(String[] args)
     {
-        int num[] = new int[10], qntPares = 0, numPares[] = new int[qntPares];
+        int num[] = new int[10];
+        ArrayList<Integer> numSelecionados = new ArrayList<>();
 
         for (int i = 0; i < num.length; i++) 
         {
             num[i] = Integer.parseInt(JOptionPane.showInputDialog(null,
-            "Informe o número " + i + 1 + ": ",
+            "Informe o número " + (i + 1) + ": ",
             "",
             JOptionPane.QUESTION_MESSAGE));
-            
         }
         for (int i = 0; i < num.length; i++) 
         {
-            if (num[i] % 2 == 0)
+            if (num[i] != 0 && num[i] % 5 == 0 && num[i] % 7 == 0) 
             {
-                
-            }
-        }
+                    numSelecionados.add(num[i]); 
 
+            }
+        }         
+        if (!numSelecionados.isEmpty())
+        {
+         System.out.printf("Foram digitados %d números divisíveis por 5 e 7\n%d -- %d -- %d", numSelecionados.size(), numSelecionados.get(0), numSelecionados.get(1), numSelecionados.get(2));
         
+        } else
+        {
+            System.out.println("Não foi digitado nenhum número divisível por 5 e 7");
+        }
     }
 }
