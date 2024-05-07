4f1f2032ad8edd486a4274921cf0729fb15b0981
DANIEL HENRIQUE DE CARVALHO TEIXEIRA ALVES <D24111@academico.domhelder.edu.br>
2024-05-07 11:43:36 -0300

    Exercício 01

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.JOptionPane;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        int num[] = new int[10], qntPares = 0, numPares[] = new int[qntPares];
+
+        for (int i = 0; i < num.length; i++) 
+        {
+            num[i] = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "Informe o número " + i + 1 + ": ",
+            "",
+            JOptionPane.QUESTION_MESSAGE));
+            
+        }
+        for (int i = 0; i < num.length; i++) 
+        {
+            if (num[i] % 2 == 0)
+            {
+                
+            }
+        }
+
+        
     }
 }
