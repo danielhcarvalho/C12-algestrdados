23aee9f5f0a340720cbb16bae501cfcd660d794e
DANIEL HENRIQUE DE CARVALHO TEIXEIRA ALVES <D24111@academico.domhelder.edu.br>
2024-05-08 10:43:53 -0300

    Documentando o exercício C12EX04

--- src/C12EX04.java
+++ src/C12EX04.java
@@ -6,17 +6,21 @@ public class C12EX04
     {
         int vencedores01[] = new int[20], corredor;
 
+        //for para receber o número dos 20 primeiros corredores a cruzar a linha de chegada
         for (int i = 0; i < vencedores01.length; i++) 
         {
             corredor = Integer.parseInt(JOptionPane.showInputDialog(null,
             "Informe o número do corredor:",
             "",
             JOptionPane.QUESTION_MESSAGE));
+
+            //array recebe o número do corredor
             vencedores01[i] = corredor;
         }
 
         System.out.println("VENCEDORES BATERIA 01: ");
 
+        //for para imprimir a posição dos corredores
         for (int i = 0; i < vencedores01.length; i++) 
         {
             System.out.printf("%d° lugar - %d\n", i + 1, vencedores01[i]);
@@ -25,6 +29,7 @@ public class C12EX04
         System.out.println();
         System.out.println("Saída de duplas: ");
 
+        //for para imprimir a orde de saida das duplas
         for (int i = 0; i < vencedores01.length; i++) 
         {
             if (i < vencedores01.length / 2)
