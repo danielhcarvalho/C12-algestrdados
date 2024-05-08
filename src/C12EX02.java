ec526eef1b83330446acc67a288c46b8ca24e992
DANIEL HENRIQUE DE CARVALHO TEIXEIRA ALVES <D24111@academico.domhelder.edu.br>
2024-05-08 11:12:53 -0300

    Documentando o exercício C12EX02

--- src/C12EX02.java
+++ src/C12EX02.java
@@ -7,8 +7,10 @@ public class C12EX02
     public static void main(String[] args) 
     {
         int num[] = new int[10];
+        //ArrayList para armazenar os números divisíveis por 5 e 7
         ArrayList<Integer> numSelecionados = new ArrayList<>();
 
+        //for para preencher o array num
         for (int i = 0; i < num.length; i++) 
         {
             num[i] = Integer.parseInt(JOptionPane.showInputDialog(null,
@@ -16,12 +18,14 @@ public class C12EX02
             "",
             JOptionPane.QUESTION_MESSAGE));
 
+            //verifica se o número é divisível por 5 e 7
             if (num[i] > 5 && num[i] < 10) 
             {
                 numSelecionados.add(num[i]);
             }
         }
 
+        //verifica se ArrayList está vazia
         if (!numSelecionados.isEmpty()) 
         {       
             System.out.print("Os números maiores que 5 e menores que 10 inseridos são: ");
@@ -33,7 +37,6 @@ public class C12EX02
                 {
                     System.out.print(", ");
                 }
-                
             }
 
         } else
