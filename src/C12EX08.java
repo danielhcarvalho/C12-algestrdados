4f1dee0633cb61cb57b50d8799e9cb643fcb8b3a
DANIEL HENRIQUE DE CARVALHO TEIXEIRA ALVES <D24111@academico.domhelder.edu.br>
2024-05-15 11:16:19 -0300

    Exercício C12EX08

--- src/C12EX08.java
+++ src/C12EX08.java
@@ -29,38 +29,46 @@ public class C12EX08
             }
         }
 
-        int divisores[] = new int[qntDivisores];
-        qntDivisores = 0;
+        if (qntDivisores != 0) 
+        {      
+            int divisores[] = new int[qntDivisores];
+            qntDivisores = 0;
 
-        //armazena os divisores da soma presentes na array numeros em uma array divisores
-        for (int i = 0; i < numeros.length; i++) 
-        {
-            if (numeros[i] != 0 && soma % numeros[i] == 0)
+            //armazena os divisores da soma presentes na array numeros em uma array divisores
+            for (int i = 0; i < numeros.length; i++) 
             {
-                divisores[qntDivisores] = numeros[i];
-                qntDivisores++;
+                if (numeros[i] != 0 && soma % numeros[i] == 0)
+                {
+                    divisores[qntDivisores] = numeros[i];
+                    qntDivisores++;
+
+                
+                }
+            }
+            //cria uma tabela para imprimir o resultado da soma e seus divisores presentes na array numeros
+            System.out.printf("""
+            ----------------------------------------
+            |   RESULTADO SOMA   |    DIVISORES    |
+            ----------------------------------------
+            |%20d|%17d|
+            ----------------------------------------
+            """, soma, divisores[0]);;
+
 
             
+            for (int i = 1; i < divisores.length; i++) 
+            {
+                System.out.printf("""
+                \t\t     |%17d|
+                \t\t     -------------------
+                """, divisores[i]);
             }
-        }
-        //cria uma tabela para imprimir o resultado da soma e seus divisores presentes na array numeros
-        System.out.printf("""
-        ----------------------------------------
-        |   RESULTADO SOMA   |    DIVISORES    |
-        ----------------------------------------
-        |%20d|%17d|
-        ----------------------------------------
-        """, soma, divisores[0]);;
 
-
-        
-        for (int i = 1; i < divisores.length; i++) 
+        } else 
         {
-            System.out.printf("""
-            \t\t     |%17d| %d
-            \t\t     -------------------
-            """, divisores[i]);
+            System.out.println("Nenhum divisor foi informado da soma total foi informado");
         }
+        
     }
 
 }
